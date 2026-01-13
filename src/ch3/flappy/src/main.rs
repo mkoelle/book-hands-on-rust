0d707c1191d0d6412ead2997126df1da50bc04d2
Mark Koelle <markkoelle@northwesternmutual.com>
2026-01-12 18:58:41 -0600

    init ch3

---
+++ src/ch3/flappy/src/main.rs
@@ -0,0 +1,16 @@
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Flappy!");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50().with_title("Flappy").build()?;
+
+    main_loop(context, State {})
+}
